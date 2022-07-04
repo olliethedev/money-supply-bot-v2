ff9c4b5c76aa195eb11ee659eec55ba33aae8acf
olliethedev <3martynov@gmail.com>
2022-07-04 19:15:34 -0400

    clean up

--- utils/screenshotHelper.ts
+++ utils/screenshotHelper.ts
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch';
 
 export const getScreenshot = async (url: string) => {
-    const fullUrl = `${process.env.URL_TO_IMAGE_CONVERTER_API}/?url=${encodeURIComponent(url)}`;
+    const fullUrl = `${ process.env.URL_TO_IMAGE_CONVERTER_API }/?url=${ encodeURIComponent(url) }`;
     console.log(fullUrl);
     const response = await fetch(fullUrl);
-    const buffer =  Buffer.from(await response.arrayBuffer());
+    const buffer = Buffer.from(await response.arrayBuffer());
     return buffer;
 }
