e99964036e8755e734bb1ecaa8e0a399ca7f5e66
olliethedev <3martynov@gmail.com>
2022-07-04 10:31:14 -0400

    followup

--- pages/api/slack/install.ts
+++ pages/api/slack/install.ts
@@ -4,14 +4,15 @@ import { getSlackInstaller } from '../../../utils';
 import handler from '../../../middlewares';
 
 
-handler.get(async (
+handler.all(async (
     req: NextApiRequestWithMongoDB,
     res: NextApiResponse<any>
 )=> {
-    await  getSlackInstaller(req.db).handleInstallPath(req, res, undefined, {
+    const data = await getSlackInstaller(req.db).handleInstallPath(req, res, undefined, {
         scopes: ['chat:write', 'chat:write.public', 'app_mentions:read'],
         metadata: 'some_metadata',
         redirectUri: process.env.SLACK_OAUTH_REDIRECT as string
     });
+    console.log({ data });
 })
 export default handler;
